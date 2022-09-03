0a32f8f3dc71c277b15162ae574fb1a9801a1e6a
manoj_pennada <manojpennada@gmail.com>
2022-09-03 10:39:03 +0530

    Quick Sort
    Best Case	:        O(n*logn)
    Average Case:	 O(n*logn)
    Worst Case:	 O(n2)

---
+++ quickSorting.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class quickSorting {
+    public static int partiton(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pidx = partiton(arr, low, high);
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        quickSort(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
